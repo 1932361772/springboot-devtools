4357b688cf639aea81e52991d2bb6d67893f6bf6
'1932361772' <'1932361772@qq.com'>
2017-07-26 19:12:05 +0800

    热部署springboot-devtools

---
+++ src/main/java/com/itmuch/cloud/HelloWordContrllor.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordContrllor {
+	@RequestMapping("/hello2")
+	public String hello() {
+		System.err.println("hello word222..........");
+		return "Hello word2223332... run as application.";
+	}
+	@RequestMapping("/hello3")
+	public String hello3() {
+		System.err.println("hello word3..........");
+		return "Hello word3... run as application.";
+	}
+
+
+}
